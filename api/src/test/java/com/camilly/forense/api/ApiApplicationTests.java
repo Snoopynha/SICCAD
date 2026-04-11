cb223ebd182273ae4b49765a2d80ea72fecd7f96
Camilly Vitória <vitoriacamilly308@gmail.com>
2026-04-11 11:54:48 -0300

    Primeiro commit

---
+++ api/src/test/java/com/camilly/forense/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camilly.forense.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
