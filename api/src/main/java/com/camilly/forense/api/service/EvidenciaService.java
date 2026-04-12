74f8f7bc9004ccfa9cde2a9fde4c76d4a8ac248f
Camilly Vitória <vitoriacamilly308@gmail.com>
2026-04-12 03:35:30 -0300

    feat(service): implementa os serviços de evidência e histórico de custódia

---
+++ api/src/main/java/com/camilly/forense/api/service/EvidenciaService.java
@@ -0,0 +1,87 @@
+package com.camilly.forense.api.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import com.camilly.forense.api.model.*;
+import com.camilly.forense.api.model.enums.StatusEvidencia;
+import com.camilly.forense.api.repository.EvidenciaRepository;
+
+@Service
+@RequiredArgsConstructor
+public class EvidenciaService {
+    private final EvidenciaRepository evidenciaRepository;
+    private final CasoService casoService;
+    private final UsuarioService usuarioService;
+    private final HistoricoCustodiaService historicoCustodiaService;
+    // Para identificar onde deve guardar localmente as evidências
+    private final String DIRETORIO_UPLOADS = "uploads_evidencias";
+
+    @Transactional
+    public Evidencia cadastrarEvidencia(Long idCaso, Long idUsuarioLogado, MultipartFile arquivo) throws Exception {
+        Caso caso = casoService.buscarPorId(idCaso);
+        Usuario perito = usuarioService.buscarPorId(idUsuarioLogado);
+
+        String hash = calcularHashSha256(arquivo.getBytes());
+        String caminhoSalvo = salvarArquivoNoDispositivo(arquivo, hash);
+
+        Evidencia evidencia = new Evidencia();
+        evidencia.setCaso(caso);
+        evidencia.setNomeOriginalArquivo(arquivo.getOriginalFilename());
+        evidencia.setTamanhoBytes(arquivo.getSize());
+        evidencia.setHashSha256(hash);
+        evidencia.setCaminhoArquivo(caminhoSalvo);
+        evidencia.setDataUpload(LocalDateTime.now());
+        evidencia.setStatus(StatusEvidencia.APREENDIDA);
+        evidencia.setCustodianteAtual(perito);
+
+        Evidencia evidenciaSalva = evidenciaRepository.save(evidencia);
+        historicoCustodiaService.registrarAcaoInicial(evidenciaSalva, perito, "Coleta inicial realizada, evidência salva no sistema");
+
+        return evidenciaSalva;
+    }
+
+    public Evidencia buscarPorId(Long id) {
+        return evidenciaRepository.findById(id).orElseThrow(() -> new RuntimeException("Evidência não encontrada"));
+    }
+
+    // Métodos auxiliares
+    private String calcularHashSha256(byte[] bytes) throws NoSuchAlgorithmException {
+        // MessageDigest gera hash de dados
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedhash = digest.digest(bytes);
+
+        // Transforma os bytes em uma String hexadecimal
+        StringBuilder hexString = new StringBuilder(2 * encodedhash.length);
+        for (byte b: encodedhash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    private String salvarArquivoNoDispositivo(MultipartFile arquivo, String hash) throws IOException {
+        Path pasta = Paths.get(DIRETORIO_UPLOADS);
+        if(!Files.exists(pasta)) {
+            Files.createDirectories(pasta);
+        }
+
+        String nome = hash.substring(0, 8) + "_" + arquivo.getOriginalFilename();
+        Path caminhoDestino = pasta.resolve(nome);
+        arquivo.transferTo(caminhoDestino);
+
+        return caminhoDestino.toString();
+    }
+}
---
+++ api/src/main/java/com/camilly/forense/api/service/HistoricoCustodiaService.java
@@ -0,0 +1,146 @@
+package com.camilly.forense.api.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import java.time.LocalDateTime;
+
+import com.camilly.forense.api.model.*;
+import com.camilly.forense.api.model.enums.AcaoCustodia;
+import com.camilly.forense.api.model.enums.StatusEvidencia;
+import com.camilly.forense.api.repository.HistoricoCustodiaRepository;
+import com.camilly.forense.api.repository.EvidenciaRepository;
+
+@Service
+@RequiredArgsConstructor
+public class HistoricoCustodiaService {
+    private final HistoricoCustodiaRepository historicoCustodiaRepository;
+    private final UsuarioService usuarioService;
+    private final EvidenciaRepository evidenciaRepository;
+
+    @Transactional
+    public void registrarAcaoInicial(Evidencia evidencia, Usuario perito, String justificativa) {
+        HistoricoCustodia log = new HistoricoCustodia();
+        log.setEvidencia(evidencia);
+        log.setCustodianteAnterior(null);
+        log.setCustodiantePosterior(perito);
+        log.setAcao(AcaoCustodia.COLETA);
+        log.setJustificativa(justificativa);
+        log.setDataHora(LocalDateTime.now());
+        log.setValidacaoHash(evidencia.getHashSha256());
+
+        historicoCustodiaRepository.save(log);
+    }
+
+    @Transactional
+    public HistoricoCustodia transferirCustodia(Long idEvidencia, Long idUsuarioOrigem, Long idUsuarioDestino, String justificativa) {
+        Evidencia evidencia = evidenciaRepository.findById(idEvidencia).orElseThrow(() -> new RuntimeException("Evidência não encontrada"));
+
+        if (!evidencia.getCustodianteAtual().getId().equals(idUsuarioOrigem)) {
+            throw new RuntimeException("Você não é o custodiante atual dessa evidência. Transferência bloqueada");
+        }
+
+        if (evidencia.getCustodianteAtual() == null) {
+            throw new RuntimeException("Essa evidência não possui um custodiante ativo");
+        }
+
+        Usuario usuarioOrigem = usuarioService.buscarPorId(idUsuarioOrigem);
+        Usuario usuarioDestino = usuarioService.buscarPorId(idUsuarioDestino);
+
+        HistoricoCustodia transacao = new HistoricoCustodia();
+        transacao.setEvidencia(evidencia);
+        transacao.setCustodianteAnterior(usuarioOrigem);
+        transacao.setCustodiantePosterior(usuarioDestino);
+        transacao.setAcao(AcaoCustodia.TRANSFERENCIA);
+        transacao.setJustificativa(justificativa);
+        transacao.setDataHora(LocalDateTime.now());
+        transacao.setValidacaoHash(evidencia.getHashSha256());
+
+        HistoricoCustodia logSalvo = historicoCustodiaRepository.save(transacao);
+
+        evidencia.setCustodianteAtual(usuarioDestino);
+        evidencia.setStatus(StatusEvidencia.EM_ANALISE);
+        evidenciaRepository.save(evidencia);
+
+        return logSalvo;
+    }
+
+
+    @Transactional
+    public HistoricoCustodia registrarAnalise(Long idEvidencia, Long idUsuarioLogado, String justificativa) {
+        Evidencia evidencia = evidenciaRepository.findById(idEvidencia).orElseThrow(() -> new RuntimeException("Evidência não encontrada"));
+
+        if (!evidencia.getCustodianteAtual().getId().equals(idUsuarioLogado)) {
+            throw new RuntimeException("Apenas o custodiante atual pode iniciar a análise dessa evidência");
+        }
+
+        HistoricoCustodia log = new HistoricoCustodia();
+        log.setEvidencia(evidencia);
+        log.setCustodianteAnterior(evidencia.getCustodianteAtual());
+        log.setCustodiantePosterior(evidencia.getCustodianteAtual());
+        log.setAcao(AcaoCustodia.ANALISE);
+        log.setJustificativa(justificativa);
+        log.setDataHora(LocalDateTime.now());
+        log.setValidacaoHash(evidencia.getHashSha256());
+
+        HistoricoCustodia logSalvo = historicoCustodiaRepository.save(log);
+
+        evidencia.setStatus(StatusEvidencia.EM_ANALISE);
+        evidenciaRepository.save(evidencia);
+
+        return logSalvo;
+    }
+
+
+    @Transactional
+    public HistoricoCustodia registrarDevolucao(Long idEvidencia, Long idUsuarioLogado, String justificativa) {
+        Evidencia evidencia = evidenciaRepository.findById(idEvidencia).orElseThrow(() -> new RuntimeException("Evidência não encontrada"));
+
+        if (!evidencia.getCustodianteAtual().getId().equals(idUsuarioLogado)) {
+            throw new RuntimeException("Apenas o custodiante atual pode devolver essa evidência");
+        }
+
+        HistoricoCustodia log = new HistoricoCustodia();
+        log.setEvidencia(evidencia);
+        log.setCustodianteAnterior(evidencia.getCustodianteAtual());
+        log.setCustodiantePosterior(null);
+        log.setAcao(AcaoCustodia.DEVOLUCAO);
+        log.setJustificativa(justificativa);
+        log.setDataHora(LocalDateTime.now());
+        log.setValidacaoHash(evidencia.getHashSha256());
+
+        HistoricoCustodia logSalvo = historicoCustodiaRepository.save(log);
+
+        evidencia.setCustodianteAtual(null);
+        evidencia.setStatus(StatusEvidencia.DEVOLVIDA);
+        evidenciaRepository.save(evidencia);
+
+        return logSalvo;
+    }
+
+    @Transactional
+    public HistoricoCustodia registrarDescarte(Long idEvidencia, Long idUsuarioLogado, String justificativa) {
+        Evidencia evidencia = evidenciaRepository.findById(idEvidencia).orElseThrow(() -> new RuntimeException("Evidência não encontrada"));
+
+        if (!evidencia.getCustodianteAtual().getId().equals(idUsuarioLogado)) {
+            throw new RuntimeException("Apenas o custodiante atual pode descartar essa evidência");
+        }
+
+        HistoricoCustodia log = new HistoricoCustodia();
+        log.setEvidencia(evidencia);
+        log.setCustodianteAnterior(evidencia.getCustodianteAtual());
+        log.setCustodiantePosterior(null);
+        log.setAcao(AcaoCustodia.DEVOLUCAO);
+        log.setJustificativa(justificativa);
+        log.setDataHora(LocalDateTime.now());
+        log.setValidacaoHash(evidencia.getHashSha256());
+
+        HistoricoCustodia logSalvo = historicoCustodiaRepository.save(log);
+
+        evidencia.setCustodianteAtual(null);
+        evidencia.setStatus(StatusEvidencia.DESCARTADA);
+        evidenciaRepository.save(evidencia);
+        
+        return logSalvo;
+    }
+}
